b3a3b6ccacfcd7e3ee4aef6c87adf880bdcc3ba3
Wagner Ferreira <wagner-ferreira09@hotmail.com>
2023-01-06 00:35:27 -0300

    Primeira Migration

---
+++ NBAStatsAPI/NBAStatsAPI.Infra/Migrations/20230106033224_Inicial.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NBAStatsAPI.Infra.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Posicoes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(30)", maxLength: 30, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Abreviacao = table.Column<string>(type: "varchar(3)", maxLength: 3, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posicoes", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Times",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(50)", maxLength: 50, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Times", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Jogadores",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PPG = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    PontosTotais = table.Column<long>(type: "bigint", nullable: false),
+                    APG = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    AssistenciasTotais = table.Column<long>(type: "bigint", nullable: false),
+                    RPG = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    RebotesTotais = table.Column<long>(type: "bigint", nullable: false),
+                    BPG = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    BlocksTotais = table.Column<long>(type: "bigint", nullable: false),
+                    SPG = table.Column<decimal>(type: "decimal(3,1)", nullable: false),
+                    StealsTotais = table.Column<long>(type: "bigint", nullable: false),
+                    PosicaoId = table.Column<long>(type: "bigint", nullable: false),
+                    TimeId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jogadores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Posicoes_PosicaoId",
+                        column: x => x.PosicaoId,
+                        principalTable: "Posicoes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Jogadores_Times_TimeId",
+                        column: x => x.TimeId,
+                        principalTable: "Times",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_PosicaoId",
+                table: "Jogadores",
+                column: "PosicaoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Jogadores_TimeId",
+                table: "Jogadores",
+                column: "TimeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jogadores");
+
+            migrationBuilder.DropTable(
+                name: "Posicoes");
+
+            migrationBuilder.DropTable(
+                name: "Times");
+        }
+    }
+}
